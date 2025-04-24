742f166dc320d35d05134d0b7df937cbd85dc4b9
OttoHawkins <djundeadvsmaxa@gmail.com>
2025-04-24 20:17:27 +0300

    fix

--- Assets/script_my/CameraFollow.cs
+++ Assets/script_my/CameraFollow.cs
@@ -4,17 +4,14 @@ using UnityEngine;
 
 public class CameraFollow : MonoBehaviour
 {
-    public Transform doodlePos; // позиция дудлика
+    public Transform doodlePos;
 
     void Update()
     {
-        if (doodlePos.position.y > transform.position.y) // если позиция дудлика больше позициии камеры
+        if (doodlePos.position.y > transform.position.y)
         {
-            // то позиция камеры приравнивается к позиции дудлика
             transform.position = new Vector3(transform.position.x, doodlePos.position.y, transform.position.z); 
         }
     }
 
-    // Подписывайся на канал ICE CREAM
-    // Нашел неточность - напиши мне на почту или в комменты! Пасибос)
 }
